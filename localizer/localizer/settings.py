95bd9ae8a79e0c037dba593df9e64ae5cbc15087
Ayushi-Jain <j.ayushi0903@gmail.com>
2016-03-30 19:11:32 +0530

    Database modifications

--- localizer/localizer/settings.py
+++ localizer/localizer/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'localizer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'localizer_db',
-        'USER': 'admin_db',
-        'PASSWORD': 'sysadmin',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'USER': 'admin_db',
+        #'PASSWORD': 'sysadmin',
+        #'HOST': 'localhost',
+        #'PORT': '',
     }
 }
 
